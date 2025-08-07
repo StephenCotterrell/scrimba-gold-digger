c2a01286d8158c3335b4e847253d57fcfc18624a
Stephen Cotterrell <stephen@cotterrell.dev>
2025-08-07 16:27:54 -0700

    added form logic to show dialog box and to close modal on buton click

--- public/index.js
+++ public/index.js
@@ -1,12 +1,17 @@
 const eventSource = new EventSource("/api/goldprice")
 
 const liveContainer = document.getElementById('price-display')
+const investmentForm = document.querySelector('form')
+const dialog = document.querySelector('dialog')
+let currentGoldPrice = null
 
 // Handle live price updates
+
 
 eventSource.onmessage = (event) => {
     const data = JSON.parse(event.data)
     const price = data.price 
+    currentGoldPrice = parseFloat(price)
     liveContainer.textContent = price
 }
 
@@ -15,3 +20,30 @@ eventSource.onmessage = (event) => {
 eventSource.onerror = () => {
     console.log("Connetion lost. Attempting to connect...")
 }
+
+investmentForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    new FormData(investmentForm)
+})
+
+investmentForm.addEventListener('formdata', (e) => {
+    const data = e.formData
+    const raw = e.formData.get('investment-amount')
+    const amount = parseFloat(raw)
+    
+    if (!isNaN(amount) && !isNaN(currentGoldPrice)) {
+        console.log(`User wants to invest ${amount}`)
+        console.log(`Current gold price $${currentGoldPrice}`)
+
+        const summary = document.getElementById('investment-summary')
+        const ounces = (amount / currentGoldPrice).toFixed(3)
+
+        summary.textContent = `You just purchased ${ounces} ounces of gold for $${amount.toFixed(2)}. You will receieve documentation shortly.`
+
+        dialog.showModal()
+        dialog.querySelector('button').addEventListener('click',() => dialog.close())
+    }
+
+    data.append('gold-price', currentGoldPrice)
+
+})
