6f231cfb25a305a856a56f68d9b4d314791befa6
Stephen Cotterrell <stephen@cotterrell.dev>
2025-08-07 12:36:35 -0700

    Created sendResponse helper function for server.js and separated out into sendResponse.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import http from 'node:http'
 import path from 'node:path'
 import fs from 'node:fs/promises'
 import { getContentType } from './utils/getContentType.js'
+import { sendResponse } from './utils/sendResponse.js'
 
 const PORT = 8000
 
@@ -20,24 +21,15 @@ const server = http.createServer( async (req, res) => {
 
     try {
         const content = await fs.readFile(filePath)
-        res.statusCode = 200
-        res.setHeader('Content-Type', contentType)
-        res.end(content)
-    } catch (err) {
+        sendResponse(res, 200, contentType, content)
+    } catch (err) { 
         if (err.code === 'ENOENT') {
             const content = await fs.readFile(path.join(publicDir, '404.html'))
-            res.statusCode = 404
-            res.setHeader('Content-Type','text/html')
-            res.end(content)
+            sendResponse(res, 404, 'text/html', content)
         } else {
-            res.statusCode = 500
-            res.setHeader('Content-Type', 'text/html')
-            res.end(`<html><h1>Server Error: ${err} </h1></html>`)
+            sendResponse(res, 500, 'text/html', `<html><h1>Server Error: ${err} </h1></html>`)
         }
     }
-
-    
-    
 })
 
 server.listen(PORT, () => {
