53fb84019363330640046cfc4f1274f9b46bea0e
Stephen Cotterrell <stephen@cotterrell.dev>
2025-08-07 12:45:58 -0700

    Separated out serveStatic logic into helper function in serveStatic.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import path from 'node:path'
 import fs from 'node:fs/promises'
 import { getContentType } from './utils/getContentType.js'
 import { sendResponse } from './utils/sendResponse.js'
+import { serveStatic } from './utils/serveStatic.js'
 
 const PORT = 8000
 
@@ -10,26 +11,8 @@ const __dirname = import.meta.dirname
 
 const server = http.createServer( async (req, res) => {
 
-    const publicDir = path.join(__dirname, 'public')
-    const filePath = path.join(
-        publicDir, 
-        req.url === '/' ? 'index.html' : req.url
-    )
-
-    const ext = path.extname(filePath)
-    const contentType = getContentType(ext)
-
-    try {
-        const content = await fs.readFile(filePath)
-        sendResponse(res, 200, contentType, content)
-    } catch (err) { 
-        if (err.code === 'ENOENT') {
-            const content = await fs.readFile(path.join(publicDir, '404.html'))
-            sendResponse(res, 404, 'text/html', content)
-        } else {
-            sendResponse(res, 500, 'text/html', `<html><h1>Server Error: ${err} </h1></html>`)
-        }
-    }
+    return await serveStatic(req, res, __dirname)
+    
 })
 
 server.listen(PORT, () => {
